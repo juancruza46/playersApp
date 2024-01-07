aaf9b4342e0397fb84b836793919d7b3cd0cde4a
juancruza46 <juan.cruza46@gmail.com>
2024-01-07 17:43:52 -0500

    home page works, all players works, specify instead of random//10 calls per min api

--- controllers/allPlayersController.js
+++ controllers/allPlayersController.js
@@ -1,45 +1,40 @@
-//all players controller
 const axios = require('axios');
 const Player = require('../models/player');
 
-class allPlayersController {
+class AllPlayersController {
     async getAllPlayers(req, res) {
         try {
-            const randomPlayerIds = 
-                [44, 49, 3218, 
-                39635, 1556, 125010, 
-                133860, 16, 3306  ];
-            const randomPlayers = [];
+            //call specific players to work with
+            const specificPlayerIds = [44, 49, 3218, 39635, 1556, 125010];
+            const specificPlayers = [];
 
-            for (const id of randomPlayerIds) {
+            for (const id of specificPlayerIds) {
                 try {
                     const apiResponse = await axios.get(`${process.env.API_BASE_URL}/v4/persons/${id}`, {
                         headers: { 'X-Auth-Token': process.env.FOOTBALL_API_KEY },
                     });
 
                     const player = apiResponse.data;
-                    randomPlayers.push({
-                         name: player.name, 
-                         position: player.position,
-                         nationality: player.nationality,
-                         shirtNumber:player.shirtNumber,
-                        });
+                    specificPlayers.push({
+                        name: player.name,
+                        position: player.position,
+                        nationality: player.nationality,
+                        shirtNumber: player.shirtNumber,
+                    });
                 } catch (error) {
                     // Handle 404 or other errors gracefully
                     console.error(`Error fetching player with ID ${id}: ${error.message}`);
                 }
-                //so my computer wont blow up in my face
-                await new Promise(resolve => setTimeout(resolve, 1000)); // 1-second delay
             }
-                        // Render the view with the fetched players
-            res.render('allPlayers', { title: 'All Players', randomPlayers });
+
+            // Render the view with the fetched players
+            res.render('allPlayers', { title: 'All Players', specificPlayers });
         } catch (error) {
             // Handle error
             console.error(error);
             res.status(500).render('error', { title: 'Error', error });
         }
     }
+}
 
-  }
-
-module.exports = new allPlayersController();
+module.exports = new AllPlayersController();
